052c5ff6c2ed595a907707c4ac37d68a4b284655
Daniel Fromer <daniel.fromer1@gmail.com>
2025-06-09 17:47:42 +0300

    Create folder by name Logic in her have a func about identification and create folder about secret code

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Malshinon.Logic;
+using Malshinon.Mdels;
+using Malshinon.DAL;
+using MySql.Data.MySqlClient;
 
 namespace Malshinon
 {
@@ -10,6 +14,13 @@ namespace Malshinon
     {
         static void Main(string[] args)
         {
+
+            
+
+
+
         }
+
+
     }
 }

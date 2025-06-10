ccaca732e0ea852c295d5772327a927df77d6125
Daniel Fromer <daniel.fromer1@gmail.com>
2025-06-10 10:13:42 +0300

    Create IntelReportdal

--- DAL/IntelReportDAL.cs
+++ DAL/IntelReportDAL.cs
@@ -15,6 +15,44 @@ namespace Malshinon.DAL
         {
             // Constructor
         }
+        public IntelReport GetReportById(string reportId)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(_connStr))
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM intel_reports WHERE report_id = @reportId";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@reportId", reportId);
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                return new IntelReport
+                                {
+                                    Id = reader.GetInt32("id"),
+                                    ReportId = reader.GetString("report_id"),
+                                    TargetId = reader.GetString("target_id"),
+                                    Text = reader.GetString("text"),
+                                    Timestamp = reader.GetDateTime("timestamp")
+                                };
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error retrieving report: {ex.Message}");
+            }
+            return null;
+
+
+        }
 
     }
 }
+
+
